0008bf08fe605243b08476072b43497954e246ab
Eric Tiancheng Gu <44741174+ericygu@users.noreply.github.com>
2020-12-07 01:17:07 -0500

    Update R Playground.R

--- R Playground.R
+++ R Playground.R
@@ -5,3 +5,5 @@ summary(DOHM)
 # Get Specific Cuisines
 chinese_cuisine <- subset(DOHM, DOHM$CUISINE.DESCRIPTION == "Chinese")
 hybrid_cuisine <- subset(DOHM, DOHM$CUISINE.DESCRIPTION == c("Chinese", "Japanese"))
+
+# This file shows us that our final project file can actually be drastically shortened by using vectors
